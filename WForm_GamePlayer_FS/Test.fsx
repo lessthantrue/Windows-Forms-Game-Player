638cc00114d6fe719c76542ea296beccba458b9f
Nick Milford <nickm@outofthisworld.net>
2016-09-25 20:23:51 -0700

    I think the F# player works satisfactory

--- WForm_GamePlayer_FS/Test.fsx
+++ WForm_GamePlayer_FS/Test.fsx
@@ -8,6 +8,6 @@ open Draw.Shapes
 
 let obj = circle (Fill Brushes.Red) 20.
 
-let g = new QuickGame<double>(initial = 0., tick = ((+) 0.1), draw = (fun s -> Transform.translate s s >! obj))
+let g = new QuickGame<double>(initial = 0., tick = ((+) 1.), draw = (fun s -> Transform.translate s s >! obj))
 startGame 500 500 10 g
 |> Observable.add (fun s -> printfn "%A" s)
