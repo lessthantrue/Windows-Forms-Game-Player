1d8b2fb4dcd1a4f5720eca1d85ca885a3484fd14
Nick Milford <nickm@outofthisworld.net>
2016-12-05 21:01:23 -0800

    Changing the test for the FS player
    adding key and mouse to the test

--- WForm_GamePlayer_FS/Test.fsx
+++ WForm_GamePlayer_FS/Test.fsx
@@ -1,13 +1,45 @@
 ﻿#load "Script.fsx"
 
 open System.Windows.Forms.GamePlayerFS
+open System.Windows.Forms
 open System.Drawing
 open gameControl
 open Draw
 open Draw.Shapes
 
-let obj = circle (Fill Brushes.Red) 20.
+let kobj = circle (Fill Brushes.Red) 20.
+let mobj color = circle (Fill color) 20.
 
-let g = new QuickGame<double>(initial = 0., tick = ((+) 1.), draw = (fun s -> Transform.translate s s >! obj))
+type mouseState = {loc : Point; state : Brush}
+type keyState = {loc : Point; vel : Size}
+
+type gameState = {key : keyState; mouse:mouseState}
+
+let state_init = {key = {loc = new Point(250, 250); vel = new Size(250, 250)}; mouse = {loc = new Point(250, 250); state = Brushes.Red}}
+
+let tick (state:gameState) : gameState = 
+    {state with key = {state.key with loc = state.key.loc + state.key.vel}}
+
+let draw (state:gameState) : Draw.Image.Type = 
+    Draw.overlayOffset (double state.key.loc.X) (double state.key.loc.Y) kobj Image.empty 
+    |> Draw.overlayOffset (double state.mouse.loc.X) (double state.mouse.loc.Y) (mobj state.mouse.state)
+
+let mouse (args:MouseEventArgs)(state:gameState) = 
+    let colorSwitch = function
+        | MouseButtons.Left -> Brushes.Yellow
+        | MouseButtons.Right -> Brushes.Blue
+        | _ -> state.mouse.state
+    {state with mouse = {loc = args.Location; state = colorSwitch args.Button}}
+
+let key (args:KeyEventArgs)(state:gameState) = 
+    let velSwitch = function
+        | Keys.Up -> Size(0, -5)
+        | Keys.Down -> Size(0, 5)
+        | Keys.Left -> Size(-5, 0)
+        | Keys.Right -> Size(5, 0)
+        | _ -> state.key.vel
+    {state with key = {state.key with vel = velSwitch args.KeyCode}}
+
+let g = new QuickGame<gameState>(initial = state_init, tick = tick, draw = draw, mouse = mouse, key = key)
 startGame 500 500 10 g
 |> Observable.add (fun s -> printfn "%A" s)
